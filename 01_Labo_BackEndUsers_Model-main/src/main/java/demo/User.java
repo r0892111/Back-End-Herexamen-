9ddf7f074f157d14bad80ddedbbb4d8308dbc9c7
Karel Van Ransbeeck <karel.vanransbeeck@outlook.be>
2023-08-20 19:14:39 +0200

    tests labUser werken

--- 01_Labo_BackEndUsers_Model-main/src/main/java/demo/User.java
+++ 01_Labo_BackEndUsers_Model-main/src/main/java/demo/User.java
@@ -7,12 +7,16 @@ public class User {
 
     private String name;
     private int age;
+    private String email;
+    private String password;
     private List<Integer> membershipYears = new ArrayList<Integer>();
 
-    public User(String name, int age) {
+    public User(String name, int age,String email, String password) {
         this.name = name;
         if (age >= 0) 
             this.age = age;
+        this.email = email;
+        this.password = password;
     }
 
     public int countMembershipYearsAfter1999 () {
@@ -37,7 +41,44 @@ public class User {
     }
 
     public String getName () {
-        return name;
+        return this.name;
+    }
+    public String getEmail(){
+        if(this.email.contains("@"))
+            return this.email;
+        return null;
+    }
+    public String getPassword(){
+        if(this.password.contains(" "))
+            this.password = "t";
+        return "@$-"+this.password+"&%#";
+    }
+    public int getFirstMembershipYear(){
+        if(this.membershipYears.isEmpty())
+            return 0;
+        int result = this.membershipYears.get(0);
+        for(int year:membershipYears){
+            if(year<result)
+                result = year;
+        }return result;
+    }
+    public String toString(){
+        return this.getName()+" is "+this.getAge()+" years old and has as email "+this.getEmail();
+    }
+
+    public int getNumberOfMembershipYearsIn2000(){
+        int result = 0;
+        if(this.membershipYears.contains(2000))
+            for(int year:membershipYears){
+                if(year<=2000)
+                    result += 1;
+            }return result;
+    }
+
+    public boolean isPasswordCorrect(String password){
+        if(this.password.equals(password))
+            return true;
+        return false;
     }
 
 }
--- 01_Labo_BackEndUsers_Model-main/src/test/java/demo/UserTest.java
+++ 01_Labo_BackEndUsers_Model-main/src/test/java/demo/UserTest.java
@@ -15,221 +15,221 @@ class UserTest {
     private String validEmailElke = "elke.steegmans@ucll.be";
     private String validPasswordElke = "ikgahetnietvertellenhoor";
 
-    // //constructor
-    // //happy case
-    // @Test
-    // void givenValidValues_whenCreatingUser_thenUserIsCreatedWithThoseValues() {
-    //     //when
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+    //constructor
+    //happy case
+    @Test
+    void givenValidValues_whenCreatingUser_thenUserIsCreatedWithThoseValues() {
+        //when
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
     
-    //     //then
-    //     assertNotNull(elke);
-    //     assertEquals(validNameElke, elke.getName());
-    //     assertEquals(validAgeElke, elke.getAge());
-    //     assertEquals(0, elke.countYearsOfMembership());
-    //     assertEquals(validEmailElke, elke.getEmail());
-    //     assertEquals("@$-"+validPasswordElke+"&%#", elke.getPassword());        
-    // }
+        //then
+        assertNotNull(elke);
+        assertEquals(validNameElke, elke.getName());
+        assertEquals(validAgeElke, elke.getAge());
+        assertEquals(0, elke.countYearsOfMembership());
+        assertEquals(validEmailElke, elke.getEmail());
+        assertEquals("@$-"+validPasswordElke+"&%#", elke.getPassword());        
+    }
     
-    // //constructor
-    // //unhappy case
-    // //invalid negative age
-    // @Test
-    // void givenInvalidNegativeAge_whenCreatingUser_thenUserIsCreatedWithAge0() {
-    //     //when
-    //     User elke = new User(validNameElke, -5, validEmailElke, validPasswordElke);
+    //constructor
+    //unhappy case
+    //invalid negative age
+    @Test
+    void givenInvalidNegativeAge_whenCreatingUser_thenUserIsCreatedWithAge0() {
+        //when
+        User elke = new User(validNameElke, -5, validEmailElke, validPasswordElke);
     
-    //     //then
-    //     assertNotNull(elke);
-    //     assertEquals(validNameElke, elke.getName());
-    //     assertEquals(0, elke.getAge());
-    //     assertEquals(0, elke.countYearsOfMembership());
-    //     assertEquals(validEmailElke, elke.getEmail());
-    //     assertEquals("@$-"+validPasswordElke+"&%#", elke.getPassword());        
-    // }
+        //then
+        assertNotNull(elke);
+        assertEquals(validNameElke, elke.getName());
+        assertEquals(0, elke.getAge());
+        assertEquals(0, elke.countYearsOfMembership());
+        assertEquals(validEmailElke, elke.getEmail());
+        assertEquals("@$-"+validPasswordElke+"&%#", elke.getPassword());        
+    }
     
-    // //countMembershipYearsAfter1999
-    // //happy case
-    // @Test
-    // void givenUserWithMemberschipYearsAfter1999_whenAskForMembershipYearsAfter1999_thenCorrectNumberIsReturned(){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
-    //     elke.addMembershipYear(2003);
-    //     elke.addMembershipYear(1999);
-    //     elke.addMembershipYear(2000);
+    //countMembershipYearsAfter1999
+    //happy case
+    @Test
+    void givenUserWithMemberschipYearsAfter1999_whenAskForMembershipYearsAfter1999_thenCorrectNumberIsReturned(){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+        elke.addMembershipYear(2003);
+        elke.addMembershipYear(1999);
+        elke.addMembershipYear(2000);
 
-    //     //when
-    //     int result = elke.countMembershipYearsAfter1999();
+        //when
+        int result = elke.countMembershipYearsAfter1999();
 
-    //     //then
-    //     assertEquals(2, result);
-    // }
+        //then
+        assertEquals(2, result);
+    }
 
-    // //countMembershipYearsAfter1999
-    // //unhappy case
-    // //no membership years after 1999
-    // @Test
-    // void givenUserWithNoMemberschipYearsAfter1999_whenAskForMembershipYearsAfter1999_then0IsReturned(){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
-    //     elke.addMembershipYear(1999);
-    //     elke.addMembershipYear(1978);
+    //countMembershipYearsAfter1999
+    //unhappy case
+    //no membership years after 1999
+    @Test
+    void givenUserWithNoMemberschipYearsAfter1999_whenAskForMembershipYearsAfter1999_then0IsReturned(){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+        elke.addMembershipYear(1999);
+        elke.addMembershipYear(1978);
 
-    //     //when
-    //     int result = elke.countMembershipYearsAfter1999();
+        //when
+        int result = elke.countMembershipYearsAfter1999();
 
-    //     //then
-    //     assertEquals(0, result);
-    // }
+        //then
+        assertEquals(0, result);
+    }
 
-    // //constructor
-    // //unhappy case
-    // //invalid email (no @)
-    // //TIP: go to the Java API to the String class and use the method contains
-    // @Test
-    // void givenInvalidEmail_whenCreatingUser_thenUserIsCreatedWithEmailNull() {
-    //     //when
-    //     User elke = new User(validNameElke, validAgeElke, "elke.steegmans.ucll.be", validPasswordElke);
+    //constructor
+    //unhappy case
+    //invalid email (no @)
+    //TIP: go to the Java API to the String class and use the method contains
+    @Test
+    void givenInvalidEmail_whenCreatingUser_thenUserIsCreatedWithEmailNull() {
+        //when
+        User elke = new User(validNameElke, validAgeElke, "elke.steegmans.ucll.be", validPasswordElke);
     
-    //     //then
-    //     assertNotNull(elke);
-    //     assertEquals(validNameElke, elke.getName());
-    //     assertEquals(validAgeElke, elke.getAge());
-    //     assertEquals(0, elke.countYearsOfMembership());
-    //     assertNull(elke.getEmail());
-    //     assertEquals("@$-"+validPasswordElke+"&%#", elke.getPassword());        
-    // }
+        //then
+        assertNotNull(elke);
+        assertEquals(validNameElke, elke.getName());
+        assertEquals(validAgeElke, elke.getAge());
+        assertEquals(0, elke.countYearsOfMembership());
+        assertNull(elke.getEmail());
+        assertEquals("@$-"+validPasswordElke+"&%#", elke.getPassword());        
+    }
 
-    // //constructor
-    // //unhappy case
-    // //invalid password (empty string)
-    // //TIP: go to the Java API to the String class and use the method isBlank
-    // @Test
-    // void givenInvalidPassword_whenCreatingUser_thenUserIsCreatedWithDefaultPasswordt() {
-    //     //when
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, "    ");
+    //constructor
+    //unhappy case
+    //invalid password (empty string)
+    //TIP: go to the Java API to the String class and use the method isBlank
+    @Test
+    void givenInvalidPassword_whenCreatingUser_thenUserIsCreatedWithDefaultPasswordt() {
+        //when
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, "    ");
     
-    //     //then
-    //     assertNotNull(elke);
-    //     assertEquals(validNameElke, elke.getName());
-    //     assertEquals(validAgeElke, elke.getAge());
-    //     assertEquals(0, elke.countYearsOfMembership());
-    //     assertEquals(validEmailElke, elke.getEmail());
-    //     assertEquals("@$-t&%#", elke.getPassword());        
-    // }
+        //then
+        assertNotNull(elke);
+        assertEquals(validNameElke, elke.getName());
+        assertEquals(validAgeElke, elke.getAge());
+        assertEquals(0, elke.countYearsOfMembership());
+        assertEquals(validEmailElke, elke.getEmail());
+        assertEquals("@$-t&%#", elke.getPassword());        
+    }
 
-    // //getFirstmembershipYear
-    // //happy case
-    // //TIP: go to the Java API to the ArrayList class and search for some methods that you can use
-    // @Test
-    // void givenUserWithMemberschipYears_whenAskForFirstMembershipYear_thenYearTheFarestInThePastIsReturned(){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
-    //     elke.addMembershipYear(2003);
-    //     elke.addMembershipYear(1999);
-    //     elke.addMembershipYear(2000);
+    //getFirstmembershipYear
+    //happy case
+    //TIP: go to the Java API to the ArrayList class and search for some methods that you can use
+    @Test
+    void givenUserWithMemberschipYears_whenAskForFirstMembershipYear_thenYearTheFarestInThePastIsReturned(){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+        elke.addMembershipYear(2003);
+        elke.addMembershipYear(1999);
+        elke.addMembershipYear(2000);
 
-    //     //when
-    //     int firstMembershipYear = elke.getFirstMembershipYear();
+        //when
+        int firstMembershipYear = elke.getFirstMembershipYear();
     
-    //     //then
-    //     assertEquals(1999, firstMembershipYear);
-    // }
+        //then
+        assertEquals(1999, firstMembershipYear);
+    }
     
-    // //getFirstmembershipYear
-    // //unhappy case
-    // //TIP: go to the Java API to the ArrayList class and search for some methods that you can use
-    // //no membership years
-    // @Test
-    // void givenUserWithNoMemberschipYears_whenAskForFirstMembershipYear_then0IsReturned(){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+    //getFirstmembershipYear
+    //unhappy case
+    //TIP: go to the Java API to the ArrayList class and search for some methods that you can use
+    //no membership years
+    @Test
+    void givenUserWithNoMemberschipYears_whenAskForFirstMembershipYear_then0IsReturned(){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
     
-    //     //when
-    //     int firstMembershipYear = elke.getFirstMembershipYear();
+        //when
+        int firstMembershipYear = elke.getFirstMembershipYear();
     
-    //     //then
-    //     assertEquals(0, firstMembershipYear);
-    // }
+        //then
+        assertEquals(0, firstMembershipYear);
+    }
 
-    // //toString
-    // //happy case
-    // @Test
-    // void givenValidUser_whenToString_thenUserObjectisReturnedInStringRepresentation() {
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+    //toString
+    //happy case
+    @Test
+    void givenValidUser_whenToString_thenUserObjectisReturnedInStringRepresentation() {
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
 
-    //     //when
-    //     String result = elke.toString();
+        //when
+        String result = elke.toString();
 
-    //     //then
-    //     assertEquals("Elke is 44 years old and has as email elke.steegmans@ucll.be", result);
-    // }
+        //then
+        assertEquals("Elke is 44 years old and has as email elke.steegmans@ucll.be", result);
+    }
 
-    // //getNumberOfMembershipYearsIn2000
-    // //happy case
-    // @Test
-    // void givenUserWithMemberschipYearsIn2000_whenAskForNumberOfMembershipYearsIn2000_thenCorrectNumberIsReturned(){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
-    //     elke.addMembershipYear(2003);
-    //     elke.addMembershipYear(1999);
-    //     elke.addMembershipYear(2000);
-    //     elke.addMembershipYear(1978);
-    //     elke.addMembershipYear(2023);
+    //getNumberOfMembershipYearsIn2000
+    //happy case
+    @Test
+    void givenUserWithMemberschipYearsIn2000_whenAskForNumberOfMembershipYearsIn2000_thenCorrectNumberIsReturned(){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+        elke.addMembershipYear(2003);
+        elke.addMembershipYear(1999);
+        elke.addMembershipYear(2000);
+        elke.addMembershipYear(1978);
+        elke.addMembershipYear(2023);
         
-    //     //when
-    //     int numberOfMembershipIn2000 = elke.getNumberOfMembershipYearsIn2000();
+        //when
+        int numberOfMembershipIn2000 = elke.getNumberOfMembershipYearsIn2000();
     
-    //     //then
-    //     assertEquals(3, numberOfMembershipIn2000);
-    // }
+        //then
+        assertEquals(3, numberOfMembershipIn2000);
+    }
 
 
-    // //getNumberOfMembershipYearsIn2000
-    // //unhappy case
-    // @Test
-    // void givenUserWithNoMemberschipYearsIn2000_whenAskForNumberOfMembershipYearsIn2000_then0IsReturned(){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
-    //     elke.addMembershipYear(1980);
-    //     elke.addMembershipYear(1999);
-    //     elke.addMembershipYear(1978);
-    //     elke.addMembershipYear(3000);
+    //getNumberOfMembershipYearsIn2000
+    //unhappy case
+    @Test
+    void givenUserWithNoMemberschipYearsIn2000_whenAskForNumberOfMembershipYearsIn2000_then0IsReturned(){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+        elke.addMembershipYear(1980);
+        elke.addMembershipYear(1999);
+        elke.addMembershipYear(1978);
+        elke.addMembershipYear(3000);
         
-    //     //when
-    //     int numberOfMembershipIn2000 = elke.getNumberOfMembershipYearsIn2000();
+        //when
+        int numberOfMembershipIn2000 = elke.getNumberOfMembershipYearsIn2000();
     
-    //     //then
-    //     assertEquals(0, numberOfMembershipIn2000);
-    // }
+        //then
+        assertEquals(0, numberOfMembershipIn2000);
+    }
 
-    // //isPasswordCorrect
-    // //happy case
-    // // @Test
-    // void givenValidUser_whenCheckingPasswordWithCorrectPassword_thenTrueIsReturned (){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
-
-    //     //when
-    //     boolean correctPassword = elke.isPasswordCorrect("ikgahetnietvertellenhoor");
-
-    //     //then
-    //     assertTrue(correctPassword);
-    // }
-
-    // //isPasswordCorrect
-    // //unhappy case
+    //isPasswordCorrect
+    //happy case
     // @Test
-    // void givenValidUser_whenCheckingPasswordWithIncorrectPassword_thenFalseIsReturned (){
-    //     //given
-    //     User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+    void givenValidUser_whenCheckingPasswordWithCorrectPassword_thenTrueIsReturned (){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
+
+        //when
+        boolean correctPassword = elke.isPasswordCorrect("ikgahetnietvertellenhoor");
+
+        //then
+        assertTrue(correctPassword);
+    }
+
+    //isPasswordCorrect
+    //unhappy case
+    @Test
+    void givenValidUser_whenCheckingPasswordWithIncorrectPassword_thenFalseIsReturned (){
+        //given
+        User elke = new User(validNameElke, validAgeElke, validEmailElke, validPasswordElke);
     
-    //     //when
-    //     boolean correctPassword = elke.isPasswordCorrect("rararaaikgahet");
+        //when
+        boolean correctPassword = elke.isPasswordCorrect("rararaaikgahet");
     
-    //     //then
-    //     assertFalse(correctPassword);
-    // }
+        //then
+        assertFalse(correctPassword);
+    }
 
 }
